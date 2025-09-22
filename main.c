40be9c9ff6a8627e3c56b6cc0b1abd9fec562f6c
joao sergio <joaosergio179@gmail.com>
2025-09-22 00:14:49 -0400

    modified:   main.c

--- main.c
+++ main.c
@@ -3,66 +3,73 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+// variaveis globais - por enquanto vai ficar aqui depois a gente ve 
 #define QUANTIDADE_USUARIO 5
+#define MAX_RECEITAS 100
+#define MAX_DESPESAS 100
+#define MAX_PRODUTOS 100
+#define MAX_FORNECEDORES 100
 
+//Banco de dados
 
-
-char usuariosCadastrado(){
-  
+//Usuarios
   char usuarios[5][100];
   char senhas[5][100];
+  int posicaoUsuario = 0;
 
+// Categorias
+  char categoriasDB[8][40] = {"Produto de uso interno", "Serviço", "Funcionario", "Para revenda", "Manutenção do local", "Produto", "Indefinida"};
+
+// Dados Despesas
+  char descricoesDespesasDB[MAX_DESPESAS][100];
+  float valoresDespesasDB[MAX_DESPESAS];
+  float quantidadesDespesasDB[MAX_DESPESAS];
+  char fornecedoresDespesasDB[MAX_DESPESAS];
+  char categoriasDespesasDB[MAX_DESPESAS];
+  int qtdDespesas = 0;
+
+// Dados Receitas
+
+  char descricoesReceitasDB[MAX_RECEITAS][100];
+  float valoresReceitasDB[MAX_RECEITAS];
+  float quantidadesReceitasDB[MAX_RECEITAS];
+  char fornecedoresReceitasDB[MAX_RECEITAS];
+  char categoriasReceitasDB[MAX_RECEITAS];
+  int qtdReceitas = 0;
+
+// Dados Produtos e serviços
+  char produtosServicosDB[MAX_PRODUTOS][100];
   
-}
-
-//cadastrar usuario 
-
-char cadastrarUsuario(char usuarios[][100], char senhas[][100], int posicaoUsuario) {
 
 
-  printf("Digite seu usuário: ");
-  fgets(usuarios[posicaoUsuario], 100, stdin);
-  usuarios[posicaoUsuario][strcspn(usuarios[posicaoUsuario], "\n")] = '\0'; 
+// Dados Fornecedores
+char fornecedoresDB[MAX_FORNECEDORES][100];
 
-  printf("Digite sua Senha: ");
-  fgets(senhas[posicaoUsuario], 100, stdin);
-  senhas[posicaoUsuario][strcspn(senhas[posicaoUsuario], "\n")] = '\0'; 
 
-  printf("Resumo do cadastro\n");
-  printf("usuario: %s\n", usuarios[posicaoUsuario]);
-  printf("senha: %s\n", senhas[posicaoUsuario]);
-  
-  return posicaoUsuario + 1;
+//Funções -- 
 
-  
-  
-}
+//Funcoes base 
 
-bool efeituarLogin(char usuarios[][100], char senhas[][100], char usuarioInformado[100], char senhaInformada[100], bool logado){
+  char gerenciadorCategorias(char categoriasCadastradas[][40]){
 
-  for(int i = 0; i<QUANTIDADE_USUARIO; i++) {
-
-    if(strcmp(usuarios[i], usuarioInformado) == 0){
-      printf("Usuário logado\n");
-      logado = true;
-    } 
-
-    return logado;
-
-  }
-  //return logado;
-
-}
-
-char gerenciadorCategorias(char categoriasCadastradas[][40]){
-
-  for(int i = 0; i<7; i++){
-    printf("%d - %s\n", i+1, categoriasCadastradas[i]);
-  }
+    for(int i = 0; i<7; i++){
+      printf("%d - %s\n", i+1, categoriasCadastradas[i]);
+    }
  
+  }
+
+  void listarTransacoes(char descR[][100], float valR[], int qtdR, char descD[][100], float valD[], int qtdD) {
+    printf("=== RECEITAS (%d) ===\n", qtdR);
+    for (int i = 0; i < qtdR; i++) {
+        printf("%2d) %s - R$ %.2f\n", i + 1, descR[i], valR[i]);
+    }
+    printf("=== DESPESAS (%d) ===\n", qtdD);
+    for (int i = 0; i < qtdD; i++) {
+        printf("%2d) %s - R$ %.2f\n", i + 1, descD[i], valD[i]);
+    }
 }
 
-int oqueFazerAposExecucao() {
+  int oqueFazerAposExecucao() {
   int opcaoSelecionada;
   printf("O que deseja fazer agora? [0 - Voltar ao menu principal | 1 - Recarregar menu atual | 2 - Finalizar programa]\n");
           scanf("%d", &opcaoSelecionada);
@@ -89,14 +96,235 @@ int oqueFazerAposExecucao() {
       return opcaoSelecionada;
 }
 
-int main() {
 
-  //Banco de dados
-  char categorias[8][40] = {"Produto de uso interno", "Serviço", "Funcionario", "Para revenda", "Manutenção do local", "Produto", "Indefinida"};
-  char usuarios[5][100];
-  char senhas[5][100];
-  int posicaoUsuario = 0;
+//Gerenciar usuario 
 
+  char cadastrarUsuario(char usuarios[][100], char senhas[][100], int posicaoUsuario) {
+
+ 
+
+  printf("Digite seu usuário: ");
+  fgets(usuarios[posicaoUsuario], 100, stdin);
+  usuarios[posicaoUsuario][strcspn(usuarios[posicaoUsuario], "\n")] = '\0'; 
+
+  printf("Digite sua Senha: ");
+  fgets(senhas[posicaoUsuario], 100, stdin);
+  senhas[posicaoUsuario][strcspn(senhas[posicaoUsuario], "\n")] = '\0'; 
+
+  printf("Resumo do cadastro\n");
+  printf("usuario: %s\n", usuarios[posicaoUsuario]);
+  printf("senha: %s\n", senhas[posicaoUsuario]);
+  
+  return posicaoUsuario + 1;
+
+  
+  
+}
+
+  bool efeituarLogin(char usuarios[][100], char senhas[][100], char usuarioInformado[100], char senhaInformada[100], bool logado){
+
+  for(int i = 0; i<QUANTIDADE_USUARIO; i++) {
+
+    if(strcmp(usuarios[i], usuarioInformado) == 0){
+      printf("Usuário logado\n");
+      logado = true;
+    } 
+
+    return logado;
+
+  }
+  //return logado;
+
+}
+
+//Gerenciar financas 
+
+  void cadastrarNovaDespesa() {
+
+  
+  int categoriaSelecionadaCadastrarDespesas;
+  int categoriaDaDespesaMenuCadastrarDespesas;
+  float quantidadeMenuCadastrarDespesas;
+  float valorDespesaMenuCadastrarDespesas;
+  char descricaoDespesaMenuCadastrarDespesas[100];
+  int fornecedorMenuCadastrarDespesas;
+
+  printf("Cadastrar nova despesa\n");
+  printf("Selecione a categoria: \n");
+  gerenciadorCategorias(categoriasDB);
+  scanf("%d", &categoriaSelecionadaCadastrarDespesas);
+  getchar();
+
+  printf("Valor da despesa: ");
+  scanf("%f", &valorDespesaMenuCadastrarDespesas);
+  getchar();
+
+  printf("Produto ou serviço: ");
+  fgets(descricoesDespesasDB[qtdDespesas], 100, stdin);
+  descricoesDespesasDB[qtdDespesas][strcspn(descricoesDespesasDB[qtdDespesas], "\n")] = '\0';
+
+  printf("Fornecedor: \n");
+  //chamar funcao que mostra fornecedores
+  scanf("%d", &fornecedorMenuCadastrarDespesas);
+  getchar();
+  
+  printf("Quantidade: ");
+  scanf("%f", &quantidadeMenuCadastrarDespesas);
+  getchar();
+
+  printf("Cadastrando despesa...\n");         
+  //descricoesDespesasDB[qtdDespesas][100] = descricaoDespesaMenuCadastrarDespesas;
+  valoresDespesasDB[qtdDespesas] = valorDespesaMenuCadastrarDespesas;
+  quantidadesDespesasDB[qtdDespesas] = quantidadeMenuCadastrarDespesas;
+  fornecedoresDespesasDB[qtdDespesas] = fornecedorMenuCadastrarDespesas;
+  categoriasDespesasDB[qtdDespesas] = categoriaDaDespesaMenuCadastrarDespesas;
+  qtdDespesas++;
+  printf("Despesa cadastrada\n");
+        
+}
+
+  void cadastrarNovaReceita() {
+
+ 
+  int categoriaReceitaMenuCadastrarReceitas;
+  int fornecedorMenuCadastrarReceitas;
+  float valorReceitaMenuCadastrarReceitas;
+  float quantidadeMenuCadastrarReceitas;
+  char descricaoReceitaMenuCadastrarReceitas[100];
+
+  printf("Cadastrar nova Receitas\n");
+  printf("Selecione a categoria: \n");
+  gerenciadorCategorias(categoriasDB);
+  scanf("%d", &categoriaReceitaMenuCadastrarReceitas);
+  getchar();
+
+  printf("Valor daReceitas: ");
+  scanf("%f", &valorReceitaMenuCadastrarReceitas);
+  getchar();
+
+  printf("Produto ou serviço: ");
+  fgets(descricoesReceitasDB[qtdReceitas], 100, stdin);
+  descricoesReceitasDB[qtdDespesas][strcspn(descricoesReceitasDB[qtdReceitas], "\n")] = '\0';
+
+  printf("Fornecedor: \n");
+  //chamar funcao que mostra fornecedores
+  scanf("%d", &fornecedorMenuCadastrarReceitas);
+  getchar();
+  
+  printf("Quantidade: ");
+  scanf("%f", &quantidadeMenuCadastrarReceitas);
+  getchar();
+
+  printf("Cadastrando despesa...\n");         
+  //descricoesDespesasDB[qtdDespesas][100] = descricaoDespesaMenuCadastrarDespesas;
+  valoresReceitasDB[qtdReceitas] = valorReceitaMenuCadastrarReceitas;
+  quantidadesReceitasDB[qtdReceitas] = quantidadeMenuCadastrarReceitas;
+  fornecedoresReceitasDB[qtdReceitas] = fornecedorMenuCadastrarReceitas;
+  categoriasReceitasDB[qtdReceitas] = categoriaReceitaMenuCadastrarReceitas;
+  qtdReceitas++;
+  printf("Despesa cadastrada\n");
+        
+}
+
+  float consultarSaldo(float receitas[], int qtdR, float despesas[], int qtdD) {
+    float saldo = 0.0f;
+    for (int i = 0; i < qtdR; i++) saldo += receitas[i];
+    for (int i = 0; i < qtdD; i++) saldo -= despesas[i];
+    return saldo;
+}
+
+  float calcularLucro(){
+    float saldo = consultarSaldo(valoresReceitasDB, qtdReceitas, valoresDespesasDB, qtdDespesas);
+    float receitas; 
+
+    for(int i; i<qtdReceitas; i++){
+      receitas += valoresReceitasDB[i];
+    }
+    
+    float porcentagemLucro = (saldo/receitas)*100;
+
+    if(porcentagemLucro<0){
+      printf("Houve um prejuízo de: %.2f\n", porcentagemLucro);
+    } else if(porcentagemLucro>0){
+      printf("Houve um lucro de: %.2f\n", porcentagemLucro);
+    }
+
+    return porcentagemLucro;
+  }
+
+// Gerenciar produtos
+
+  void cadastrarProduto(char descricaoProdutoServico[100]){
+    char descricao = descricaoProdutoServico;
+    for(int i = 0; i< MAX_PRODUTOS; i++ ){
+      if(!produtosServicosDB[i]|| (strcmp(produtosServicosDB[i], '\0') == 0)){
+        produtosServicosDB[i][100] = descricao; 
+        printf("produto: %s\n", produtosServicosDB[i]);
+        break;
+      }
+      break;
+    }
+  }
+
+  void excluirProduto(char descricaoProdutoServico[100]){
+    for(int i = 0; i<MAX_PRODUTOS; i++){
+      if(strcmp(descricaoProdutoServico, produtosServicosDB[i]) == 0){
+        produtosServicosDB[i][100] = '\0';
+      }
+    }
+  }
+
+  void listarProdutos(){
+    printf("Produtos cadastrados\n");
+    for(int i = 0; i<MAX_PRODUTOS; i++){
+      if(strcmp(produtosServicosDB[i], '\0') != 0 && produtosServicosDB[i]){
+        printf("%d - %s\n", i+1, produtosServicosDB[i]);
+      }
+    }
+
+  }
+
+  void gerenciadorMenuProdutoEServicos(){
+
+    int opcaoSelecionada;
+    char produto[100];
+
+    printf("O que deseja fazer agora?\n");
+    printf("1 - Listar Produtos e serviços\n");
+    printf("2 - Cadastrar um novo fonecedor\n");
+    printf("3 - Excluir um fornecedor\n");
+    scanf("%d", &opcaoSelecionada);
+    getchar();
+
+    switch (opcaoSelecionada)
+    {
+    case 1:
+      listarProdutos();
+      break;
+    case 2: 
+      printf("Descrição do produto ou serviço: ");
+      fgets(produto, sizeof(produto), stdin);
+      produto[strcspn(produto, "\n")] = '\0';
+      cadastrarProduto(produto);
+      break;
+    case 3:
+      fgets(produto, sizeof(produto), stdin);
+      produto[strcspn(produto, "\n")] = '\0';
+      excluirProduto(produto);
+      break;
+    
+    default:
+      break;
+    }
+
+  }
+
+
+//programa
+
+  int main() {
+  //Banco de dados local
+  
   //Variaveis estados do sistema
   bool cadastroEfeituado = false;
   bool usuarioLogado = false;
@@ -112,13 +340,11 @@ int main() {
   bool continuarMenuListarCategoria = true;
   bool continuarMenuDadosCadastrais = true;
   bool continuarPrograma = true;
-  bool teste;
+ 
 
   //Variaveis fluxo do sistema
   int cadastrarMaisUmUsuario = 0;
 
-
-  
   // Variaveis menu login
   char usuarioInformadoLogin[100];
   char senhaInformadaLogin[100];
@@ -127,24 +353,11 @@ int main() {
   int opcaoSelecionadaMenuPrincipal;
 
   //Variaveis menu Cadastrar despesas
-  int categoriaSelecionadaCadastrarDespesas;
-  int categoriaDaDespesaMenuCadastrarDespesas;
-  float quantidadeMenuCadastrarDespesas;
-  float valorDespesaMenuCadastrarDespesas;
-  char descricaoDespesaMenuCadastrarDespesas[100];
-  char fornecedorMenuCadastrarDespesas[100];
-
 
   //Variaveis menu cadastrar receitas
-  int categoriaSelecionadaCadastrarReceitas;
-  int categoriaDaReceitaMenuCadastrarReceitas;
-  float valorReceitaMenuCadastrarReceitas;
-  float quantidadeMenuCadastrarReceitas;
-  char descricaoReceitaMenuCadastrarReceitas[100];
-
+  
   //Variaveis menu Comparativo
   char produtoOuServicoMenuComparativo[100];
-
 
   //Variaveis menu fornecedor
   int opcaoSelecionadaMenuFornecedores;
@@ -230,65 +443,49 @@ int main() {
         continuarPrograma = false;
         break;
         case 1:
+        printf("Menu Saldo\n");
         printf("Consultado saldo...\n");
         sleep(1);
-
         // chamar funcao que consulta o saldo
-
+        float saldo = consultarSaldo(valoresReceitasDB, qtdReceitas, valoresDespesasDB, qtdDespesas);
+        printf("Saldo atual: R$ %.2f\n", saldo);
         break;
         case 2:
         do {
-
+          printf("Menu Historio\n");
           printf("Buscando suas informações...\n");
           sleep(1);
+          printf("Gerando Relatório\n");
           // chamar funcao que lista as transaçõoes
+          listarTransacoes(descricoesReceitasDB, valoresReceitasDB, qtdReceitas, descricoesDespesasDB, valoresDespesasDB, qtdDespesas);
+
+          int oqueFazerAposIsso = oqueFazerAposExecucao();
+          if(oqueFazerAposIsso == 0){
+
+            continuarMenuReceitas = false;
+
+          } else if(oqueFazerAposIsso == 1){
+
+            continuarMenuReceitas = true;
+
+          } else if (oqueFazerAposIsso == 2) {
+              continuarMenuReceitas = false;
+              continuarMenuPrincipal = false;
+              continuarPrograma=false;
+
+          } else {
+            printf("Opção invalida! Voltando ao menu principal ...\n");
+            continuarMenuReceitas = false;
+          }
+
         } while (continuarMenuHistorico);
 
         break;
         case 3: 
 
         do {
-          printf("Cadastrar nova despesa\n");
-          printf("Selecione a categoria: \n");
-          //printf("1- Produto de uso interno;\n");
-          //printf("2 - Serviço;\n");
-          //printf("3 - Funcionario;\n");
-          //printf("4 - Para revenda;\n");
-          //printf("5 - Manutenção do Local;\n");
-          //printf("6 - Produto;\n");
-          //printf("7 - Indedefinida;\n");
-          //printf("0 - Voltar ao menu principal\n");
-          // aqui da pra criar uma funcao que gerencia as categorias disponiveis
-          gerenciadorCategorias(categorias);
-          scanf("%d", &categoriaSelecionadaCadastrarDespesas);
-          getchar();
-
-          printf("Valor da despesa: ");
-          scanf("%f", &valorDespesaMenuCadastrarDespesas);
-          getchar();
-
-          printf("Produto ou serviço: ");
-          fgets(descricaoDespesaMenuCadastrarDespesas, sizeof(descricaoDespesaMenuCadastrarDespesas), stdin);
-          descricaoDespesaMenuCadastrarDespesas[strcspn(descricaoDespesaMenuCadastrarDespesas, "\n")] = '\0';
-
-          printf("Fornecedor: \n");
-          fgets(fornecedorMenuCadastrarDespesas, sizeof(fornecedorMenuCadastrarDespesas), stdin);
-          fornecedorMenuCadastrarDespesas[strcspn(fornecedorMenuCadastrarDespesas, "\n")] = '\0';
-          //aqui acho que vamos ter que mudar ainda, pq na opção 7 tem como cadastrar e excluir fornecedores, então é melhor criar uma fucão que vai retornar quais fornecedores estão ativos no momento em que for execultada.
-
-          printf("Quantidade: ");
-          scanf("%f", &quantidadeMenuCadastrarDespesas);
-          getchar();
-
-          printf("Cadastrando despesa...\n");
-          // chamar funcao que vai cadastrar a despesa
-          printf("Despesa cadastrada\n");
-
-          /*printf("O que deseja fazer agora? [0 - Voltar ao menu principal | 1 - cadastrar uma nova despesa]\n");
-          scanf("%d", &oqueFazerAposIsso);
-          getchar(); */ //funcao oqueFazerAposExecucao vai pegar isso
-        
-
+  
+          cadastrarNovaDespesa();
           int oqueFazerAposIsso = oqueFazerAposExecucao();
           if(oqueFazerAposIsso == 0){
 
@@ -316,27 +513,8 @@ int main() {
         case 4:
 
         do {
-          printf("Cadastrar uma nova receita\n");
-          printf("Valor da receita: ");
-          scanf("%f", &valorReceitaMenuCadastrarReceitas);
-          getchar();
-          printf("Quantidade: ");
-          scanf("%f", &quantidadeMenuCadastrarReceitas);
-          getchar();
-          printf("Produto ou serviço: \n");
-          fgets(descricaoReceitaMenuCadastrarReceitas, sizeof(descricaoDespesaMenuCadastrarDespesas), stdin);
-          descricaoReceitaMenuCadastrarReceitas[strcspn(descricaoReceitaMenuCadastrarReceitas, "\n")] = '\0';
-          //mesma coisa que antes acho que vamos ter que mudar ainda, pq na opção 7 tem como cadastrar e excluir fornecedores.
 
-          printf("Selecione uma categoria: \n");
-          gerenciadorCategorias(categorias);
-          //chamar funcao que vai listar as categorias disponiveis
-          scanf("%d", &categoriaDaReceitaMenuCadastrarReceitas);
-          getchar();
-
-          printf("Registrando receita ...\n");
-          //chamar funcao que vai cadastrar a receita 
-          printf("Receita cadastrada!\n");
+          cadastrarNovaReceita();
 
           int oqueFazerAposIsso = oqueFazerAposExecucao();
           if(oqueFazerAposIsso == 0){
@@ -362,10 +540,13 @@ int main() {
       
         break;
         case 5:
-        
-        printf("Consultando dados ...\n");
-        //chamar funcao que vai calcular a receita, as despesas, o lucro.
-
+        do {
+          printf("Menu Lucros e Despesas\n");
+          printf("Consultando dados ...\n");
+          sleep(1);
+          //chamar funcao que vai calcular a receita, as despesas, o lucro.
+          calcularLucro();
+        } while (continuarMenuLucrosEDespesas);
         break;
         do {
           case 6:
@@ -435,8 +616,10 @@ int main() {
         break;
         case 8:
         do{
-          printf("Menu lucros e despesas\n");
+          printf("Menu Produtos e Serviços\n");
           printf("Buscando informações...\n");
+          gerenciadorMenuProdutoEServicos();
+          
           //chamar funcão que vai lidar em calcular todas as despesas e gastos e retornar o lucro
 
           int oqueFazerAposIsso = oqueFazerAposExecucao();
@@ -464,7 +647,7 @@ int main() {
         do{
           printf("Menu listar categorias\n");
           printf("Listando categorias...\n");
-          gerenciadorCategorias(categorias);
+          gerenciadorCategorias(categoriasDB);
           printf("Fim da lista\n");
 
           int oqueFazerAposIsso = oqueFazerAposExecucao();
